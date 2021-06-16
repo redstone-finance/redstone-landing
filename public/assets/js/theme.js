62bc8ac7d6d13875d7f19bf6c65b15cce9502ed9
piotruda <piotrduda92@gmail.com>
2021-06-16 09:48:32 +0200

    Google Analytics, Cookies Consent popup (#2)
    * Google Analytics, Cookies Consent popup
    
    * Cookie consent classes quick fix
    
    * Cookie consent from CDN, periods in texts
    
    * Theme changes
    
    Co-authored-by: Piotr Duda <piotrduda1992@gmail.com>

--- public/assets/js/theme.js
+++ public/assets/js/theme.js
@@ -384,4 +384,23 @@ var scrollToTop = function scrollToTop() {
 docReady(navbarInit);
 docReady(detectorInit);
 docReady(scrollToTop);
+window.cookieconsent.initialise({
+  palette: {
+    popup: {
+      background: '#24355B',
+      text: '#FFFFFF'
+    },
+    button: {
+      background: '#FD627A',
+      text: '#FFFFFF'
+    }
+  },
+  showLink: false,
+  theme: 'classic',
+  position: 'bottom-right',
+  content: {
+    message: 'This site uses cookies to analyze traffic and offer a better browsing experience.',
+    dismiss: 'Agree'
+  }
+});
 //# sourceMappingURL=theme.js.map
