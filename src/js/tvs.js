fea243f407f0c8d7681e7159ec5054a67a611d98
cehali <cezary.haliniarz@gmail.com>
2024-07-01 10:42:36 +0200

    feat: add tooltip to tvs

--- src/js/tvs.js
+++ src/js/tvs.js
@@ -10,21 +10,29 @@ function generateTvsElement(tvs) {
         class="link-like-text-button"
       >
         <div class="py-2 px-4">
-        ${
-          tvs
-            ? `<h3>Protecting <b>${tvs} billion</b></h3>`
-            : `<div class="flex gap-5 align-items-center">
-                <h3>Protecting</h3>
-                <div class="loader"></div>
-              </div>`
-        }  
+          <div class="flex gap-2 align-items-center justify-content-center mb-2">
+            <h3 class="mb-0">Protecting</h3>
+            ${
+              tvs
+                ? `<h3 class="mb-0"><b>${tvs} billion</b></h3>`
+                : `<div class="flex align-items-center justify-content-center loader-container-tvs">
+                    <div class="loader"></div>
+                  </div>`
+            }
+            <div class="simple-tooltip">
+              <img src="/assets/img/icons/info.svg" />
+              <span class="tooltiptext">
+                TVS (Total Value Secured) is a metric similar to TVL (Total Value Locked) that also counts assets that are temporarily outside of the protocol (Borrows & Double Count) but could be lost if an oracle misprices delivered feeds.
+              </span>
+            </div>
+          </div>
           <div class="flex gap-2 align-items-center justify-md-content-left justify-content-center">
             <h6 class="m-0">Total Value Secured (TVS) by</h6>
             <img src="/assets/img/logos/defillama.svg" />
           </div>
         </div>
       </a>
-    </div`;
+    </div>`;
 }
 
 if (document.getElementById("tvs")) {
